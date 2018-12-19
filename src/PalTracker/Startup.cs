32511b6022b43a30eaee8fe743bfcfce500a3687
Douglas_Ribeiro <doug.ribeiro.poa@gmail.com>
2018-12-19 10:37:50 -0200

    ASP.NET Core Web API

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -36,6 +36,8 @@ namespace PalTracker
 
 
             services.AddSingleton(sp => new WelcomeMessage(Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")));
+
+            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
